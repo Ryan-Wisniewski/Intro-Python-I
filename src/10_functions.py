1eddee8b8daa484483e3d127afefc7d74897ff1d
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2019-12-02 21:55:24 -0500

    11/15 completed

--- src/10_functions.py
+++ src/10_functions.py
@@ -9,4 +9,10 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+def evenCheck(num):
+    if (num % 2) == 0:
+        return "Even"
+    else:
+        return "Odd"
 
+print(evenCheck(num))
--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(x, y):
+    return x + y
 
 print(f1(1, 2))
 
@@ -12,6 +14,11 @@ print(f1(1, 2))
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*args):
+    number = 0
+    for x in args:
+        number += x
+    return number
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,13 +28,21 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+
+#25 minutes for a star xDxD how asad
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(arg1, arg2 = 0):
+    if arg2 == 0:
+        return arg1 + 1
+    else:
+        return arg1 + arg2
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -42,6 +57,9 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**arg):
+    for x, y in arg.items():
+        print('key: {}, value: {}'.format(x,y))
 
 # Should print
 # key: a, value: 12
@@ -59,5 +77,5 @@ d = {
     "hp": 3
 }
 
-# What thing do you have to add to make this work?
-f4(d)
+# What thing do you have to add to make this work? ** ! this time it was a lot quicker for me to figure out
+f4(**d)
