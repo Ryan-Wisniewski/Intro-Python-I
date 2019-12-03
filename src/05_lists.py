791808069105b0453f254425ea4cd1fde7622a2a
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2019-12-02 21:56:48 -0500

    completed 11/15

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,38 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE 
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE 
+for num in y: #<-- colon != '=' ?
+    x.append(num)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE 
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE 
+
+#Awwww thought this would work
+# for num in x:
+#     print(num)
+#     if num == 9:
+#         break
+#         x.append(99)
+x.insert(5, 99)
+
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE 
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for i in x:    
+    print(i * 1000)
